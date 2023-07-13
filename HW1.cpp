b790931d0643a246c062b35a732a79074aa690e0
Michael P <30189184+MichaelPokotskyi@users.noreply.github.com>
2023-07-13 13:10:56 -0700

    upd

--- HW1.cpp
+++ HW1.cpp
@@ -1,20 +1,35 @@
+//
+// Michael Pokotskyi U09662201
+// pokotskyi.m@gmail.com
+// C I C++ Programming III CSE-40477
+// Raymond Mitchell III
+// HW1.cpp
+// Win10, Visual C++ 2022, ISO C17
+
+// Date class representing a month, day, and year with testing .
+// Extended with monthValidation() private method to reuse validation piece 
+// of code.
+
+
 #include <iostream>
 using std::cout;
 using std::cerr;
 
 class Date {
+    // default month
     const int DEFAULT_MONTH = 1;
 private:
-    // object fields
+    // class fields
     int Month;
     int Day;
     int Year;
 
-    // month validation
+    // 1-12 month validation
     void monthValidation(int Month) {
         if (Month < 1 || Month > 12) {
             this->Month = DEFAULT_MONTH;
-            cerr << "1-12 expected for Month but " << Month << " found." << "\n";
+            cerr << "1-12 expected for Month but " << Month 
+                << " found. Will be set to default value." << "\n";
         }
         else {
             this->Month = Month;
@@ -58,14 +73,24 @@ public:
 };
 
 int main() {
-    // tests
-    Date d1(12, 31, 23);
+    // tests obj instansiation
+    Date d1(12, 31, 2023); // OK values
     d1.display();
 
-    Date d2(0, 31, 23);
+    Date d2(0, 31, 2023); // test Month < 1
     d2.display();
 
-    Date d3(13, 31, 23);
+    Date d3(13, 31, 2023); // test Month > 12
     d3.display();
 
+    // test setters
+    d1.setMonth(88); // validation are OK
+    d1.setDay(88);
+    d1.setYear(8888);
+    d1.display();
+
+    // test getters
+    cout << "Get Month " << d1.getMonth() << "\n";
+    cout << "Get Day " << d1.getDay() << "\n";
+    cout << "Get Year " << d1.getYear() << "\n";
 }
