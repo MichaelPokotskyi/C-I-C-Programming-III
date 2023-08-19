be54a813b6740695e907c609ad60563ab9c96bc2
MiShKa <sly.booking@gmail.com>
2023-08-18 20:32:57 -0700

    ooo

---
+++ Array.h
@@ -0,0 +1,79 @@
+//
+// Michael Pokotskyi U09662201
+// pokotskyi.m@gmail.com
+// C I C++ Programming III CSE-40477
+// Raymond Mitchell III
+// Array.h
+// Win10, Visual C++ 2022, ISO C17
+//
+// Class Array implementation
+
+// include guard
+#ifndef ARRAY_H
+#define ARRAY_H
+
+#include <stdexcept>
+using std::invalid_argument;
+
+// defined namespace
+namespace MichaelPokotskyi
+{
+    // non-type parameters
+    template<typename ElemType, int SIZE>
+    class Array {
+    public:
+        // default constructor
+        Array() {
+
+        }
+        // copy constructor
+        Array(ElemType source[SIZE])
+            : ElemType(ElemType), elements(source) {
+        }
+        // destructor
+        ~Array() {
+        }
+        // = overloading
+        Array<ElemType, SIZE> operator=(const ElemType source[SIZE]) const {
+            ElemType Elem[SIZE];
+            for (int i = 0; i < size; i++) {
+                Elem[i] = source[i];
+            }
+            return Elem;
+        }
+        // == overloading
+        bool operator==(const Array<ElemType, SIZE> other) const {
+            for (int i = 0; i < size; i++) {
+                if (this->elements[i] != other[i];) { break; return false; }
+            }
+            return true;
+        }
+        // != overloading
+        bool operator!=(const Array<ElemType, SIZE> other) const {
+            for (int i = 0; i < size; i++) {
+                if (this->elements[i] == other[i];) { break; return false; }
+            }
+            return true;
+        }
+        //[] overloading LValue
+        ElemType operator[](int index){
+            if ((index < 0) || (index >= SIZE)) {
+                throw invalid_argument("Subscript index is out of range!");
+            }
+            return this->elements[index];
+        }
+        //[] overloading RValue
+        ElemType operator[](int index) {
+            if ((index < 0) || (index >= SIZE)) {
+                throw invalid_argument("Subscript index is out of range!");
+            }
+            return this->elements[index];
+        }
+
+    private:
+    ElemType elements[SIZE];
+
+    };
+}
+
+#endif
---
+++ HW6.cpp
@@ -0,0 +1,21 @@
+//
+// Michael Pokotskyi U09662201
+// pokotskyi.m@gmail.com
+// C I C++ Programming III CSE-40477
+// Raymond Mitchell III
+// HW6.cpp
+// Win10, Visual C++ 2022, ISO C17
+//
+// Tests for Array class implementation.
+
+
+#include <iostream>
+#include "Array.h"
+
+using std::cout;
+using std::endl;
+using namespace MichaelPokotskyi;
+
+int main() {
+    Array<int, 5> arrayOfFiveInts;
+}
